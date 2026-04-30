edd27f0a0751d9494f9626767c11a0d28f3b56d5
Renan Barbosa <renan_vianna7@icloud.com>
2026-04-30 19:09:24 -0300

    feat: implementacao inicial com pipeline compartilhado

---
+++ src/main.py
@@ -0,0 +1,14 @@
+﻿import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("ml-training-service")
+
+def main():
+    logger.info("Service ml-training-service starting...")
+    while True:
+        logger.info("Service ml-training-service is running...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
